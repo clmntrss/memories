709a524ea32b026e671e2cda0e1eaaa8a3b4c5bf
Léo Nicaisse <leonicaisse@gmail.com>
2018-03-23 22:45:17 +0100

    render

---
+++ bails Leo/jeu code/js/app.js
@@ -0,0 +1,22 @@
+$('document').ready(function () {
+    $('#obj-1').on('click', function (e) {
+        //des bails
+    });
+
+
+    let minutes = 30;
+    let seconds = 0;
+    let timeLeft = minutes * 60 + seconds;
+    let minutesLeft = minutes;
+
+    setInterval(function () {
+        let secondsLeft = timeLeft % 60;
+        $('#countdown').html(("0" + Math.floor(minutesLeft)).slice(-2) + ":" + ("0" + Math.floor(secondsLeft)).slice(-2));
+        if (timeLeft <= 300) {
+            $('#countdown').css('color', '#ff0024');
+        }
+        if (secondsLeft === 0) minutesLeft--;
+        timeLeft--;
+    }, 1000);
+})
+;
