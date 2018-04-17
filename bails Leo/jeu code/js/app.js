72e45b36b16e1f7526cdbb060ce80267e1e2dcbf
Léo Nicaisse <leonicaisse@gmail.com>
2018-04-17 14:46:39 +0200

    ajout du gameover + un peu de bails divers

--- bails Leo/jeu code/js/app.js
+++ bails Leo/jeu code/js/app.js
@@ -1,21 +1,5 @@
 $('document').ready(function () {
 
-    var minutes = 30;
-    var seconds = 0;
-    var timeLeft = minutes * 60 + seconds;
-    var minutesLeft = minutes;
-
-    setInterval(function () {
-        let secondsLeft = timeLeft % 60;
-        let countdown = $('#countdown');
-        countdown.html(("0" + Math.floor(minutesLeft)).slice(-2) + ":" + ("0" + Math.floor(secondsLeft)).slice(-2));
-        if (timeLeft <= 300) {
-            countdown.css('color', '#ff0300');
-        }
-        if (secondsLeft === 0) minutesLeft--;
-        timeLeft--;
-    }, 1000);
-
     $('#btn-answer-1').on('click', function (e) {
         e.preventDefault();
         reponse = $('#input-reponse-1').val();
