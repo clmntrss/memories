1b96c1bafb2d87419d103ef6175e01b5682e8f06
Léo Nicaisse <leonicaisse@gmail.com>
2018-04-18 16:35:15 +0200

    ajout de sons (game-over, digicode, folders)

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 $('document').ready(function () {
 
+    let gameOverSound = new Audio('audio/game-over.mp3');
     minutes = 30;
     let seconds = 0;
-    let timeLeft = minutes * 60 + seconds;
     let minutesLeft = minutes;
+    let timeLeft = minutesLeft * 60 + seconds;
     let gameOn = false;
     setInterval(function () {
         if (gameOn) {
             let secondsLeft = timeLeft % 60;
+            timeLeft = minutesLeft * 60 + secondsLeft;
             $('#countdown-inner').html(("0" + Math.floor(minutesLeft)).slice(-2) + ":" + ("0" + Math.floor(secondsLeft)).slice(-2));
             if (timeLeft <= 300) {
                 $('#countdown').css('color', '#ef0000');
             }
             if (secondsLeft === 0) minutesLeft--;
             timeLeft--;
-            if (timeLeft === -1) {
+            if (timeLeft <= -1) {
+                gameOverSound.play();
                 gameOn = false;
                 $('#gameover-overlay').css({
                     display: 'flex'
@@ -48,22 +51,40 @@ $('document').ready(function () {
             $('.enigme-btn-1').css({
                 background: 'radial-gradient(#00ff0088, rgba(255, 255, 255, 0) 65%)'
             });
-            $.fancybox.close();
             bonneReponse('enigme-1', 9);
+            closeFolder();
         } else {
             $('#info-reponse-1').css('color', 'red');
             $('#info-reponse-1').html('mauvaise réponse');
+            malus();
+
         }
     });
+
+    function bonneReponse(enigme, number) {
+        selectedEnigme = 'digit-' + enigme;
+        $('#' + selectedEnigme).css({
+            backgroundImage: "url(images/" + selectedEnigme + ".png)",
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center center',
+            backgroundSize: 'cover',
+        });
+        $('#' + selectedEnigme).html(number);
+    }
+
+    function closeFolder() {
+        $.fancybox.close();
+    }
+
+    function malus() {
+        minutesLeft--;
+        $('#malus').css("display", "inherit");
+        $('#malus').fadeIn(500).delay(1500).fadeOut(800);
+    }
 });
 
-function bonneReponse(enigme, number) {
-    selectedEnigme = 'digit-' + enigme;
-    $('#' + selectedEnigme).css({
-        backgroundImage: "url(images/" + selectedEnigme + ".png)",
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center center',
-        backgroundSize: 'cover',
-    });
-    $('#' + selectedEnigme).html(number);
+function playSound(file) {
+    var sound = new Audio(file);
+    sound.load();
+    sound.play();
 }
